19317a52033f647d1ff4c192ece0880870d54f6d
Bacon <e2067696@cmaisonneuve.qc.ca>
2024-03-07 15:27:46 -0500

    Aligné le header à gauche sur mobile, ajouté checkbox visible, et appliqué un fond en dégradé.

--- header.php
+++ header.php
@@ -12,11 +12,12 @@
 </head>
 <body>
     <div id="menu_entete" class="global">
+         <header class="menu_header">
         <input type="checkbox" name="" id="chk_burger">
         <label id="burger" for="chk_burger">
             <svg   width="32" height="32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#fff"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
         </label>  
-        <header class="menu_header">
+       
             <?php wp_nav_menu(array("container" => "nav")); ?>
         </header>
     </div>
